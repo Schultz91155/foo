7d8f1a4a0d382de655f5b343b5e605029a594f2d
Schultz91155 <37634890+Schultz91155@users.noreply.github.com>
2023-11-25 17:35:00 +0300

    Initial Commit

---
+++ foo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  foo
+//
+//  Created by Pavel Brovkin on 25.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
